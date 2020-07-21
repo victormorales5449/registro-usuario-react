11b72fb02366e3edde4177337ccb630b096a811c
Victor Morales <victormorales_1588@hotmail.com>
2020-07-21 03:04:16 -0400

    mi priemara app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi primer proyecto con react Ñ=
         </p>
         <a
           className="App-link"
